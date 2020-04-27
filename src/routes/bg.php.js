8490a6a1ce35721732974b49e19535714a4c3064
xwcoco <xwcoco@msn.com>
2020-04-27 20:53:28 +0800

    add url to background

--- src/routes/bg.php.js
+++ src/routes/bg.php.js
@@ -5,8 +5,9 @@ var logger = log4js.getLogger();
 logger.level = 'debug';
 
 router.get('/', async function (ctx, next) {
-    logger.debug(ctx.request.query);
-    ctx.body = "http://192.168.2.11:8080/backimages/bk1.png";
+    logger.debug(ctx.request);
+    ctx.body = 'http://'+ctx.request.header.host + '/backimages/bk1.png';
+    // ctx.body = "http://192.168.2.11:8080/backimages/bk1.png";
     return true;
     // ctx.body = 'demo'
 })
